156710d4161b8f430f1d5809f1c19ed79f79d1ce
wanghairong-i <3189578410@qq.com>
2017-08-15 15:35:46 +0800

    add effect up readme

--- README.md
+++ README.md
@@ -1,25 +1,43 @@
+# turnMsg - 一条接一条的平滑出现信息的过渡动画
+
 ### [点我预览](https://wanghairong-i.github.io/turnPrepend/)
 
-# turnPrepend - 一条接一条的出现信息的过渡动画
+## 介绍
+
+-   基于jQuery封装的
+-   api丰富，使用简单
+-   自适应宽度，可方便兼容到你的项目中
+-   可配置消息是向上滚动、还是向下滚动
+-   可自定义容器、列表等样式
 
 ## 使用方法
 
+-   引入 `turnMsg.js` 即可
+
 -   需要一个html容器，如:
     ```javascript
+
     <div id="idName"></div>
+
     ```
 
--   初始化
+-   创建实例
     ```javascript
-    var t = new TurnPrepend({box:"#idName"});
+
+    var t = new turnMsg("idName");
+
     ```
 
--   传入每一条信息
+-   每次传入一条信息
     ```javascrpt
-    t.setHtml({html:'<li>text</li>'});
+
+    t.addText('我是一条信息啦');
+
     ```
       
 -   如需关闭信息框时
     ```javascript
-    t.hideBox();
+
+    t.empty();
+
     ```
---
+++ turnMsg.js
@@ -0,0 +1,93 @@
+/* turnMsg.js  一条接一条的出现信息的过渡动画
+** auth:whr
+** time:2016.10.12
+** 有两个公共方法：addHtml、empty
+*/
+;(function($) { 
+	/*
+	** @param node [必传] <Str || jQuery>   滚动盒子的容器id名，可"#idName"或"idname"或jquery对象$("#idname")
+	** @param config [选传] <Obj>
+	** 		- boxStyle      <Obj>   //最外层容器样式
+	** 		- middleStyle   <Obj>   //包列表项的容器样式
+	** 		- middleTag     <Str>   //包列表项的容器标签
+	** 		- itemTag       <Str>   //列表项的标签
+	** 		- itemHeight    <Num>   //每一项的高度
+	** 		- to            <Str>   //滚动方向，'bottom'向下滚动消息，'top'向下滚动消息
+	** 		- max           <Num>   //容器最多条数，超过后将自动清空
+	** @param callback [选传] <Func> 初始化完成后的回调，两个参数，第一个是盒子容器的jQ对象，第二个是config对象
+	*/
+	function turnMsg(node, config, callback) {
+		config = config || {};
+		var $box = typeof node === 'string' && node.indexOf('#') !== 0 ? $(document.getElementById(node)) : $(node);
+		var boxStyle = $.extend({
+			position: 'relative',
+			width: '100%',
+			height: 0,
+			overflow: 'hidden',
+		}, config.boxStyle);
+		var middleStyle = $.extend({
+			position: 'absolute',
+			left: 0,
+			'-webkit-transition': 'all .5s',
+			'-moz-transition': 'all .5s',
+			'-ms-transition': 'all .5s',
+			'-o-transition': 'all .5s',
+			transition: 'all .5s',
+			zIndex: 20,
+		}, config.middleStyle);
+		this.config = $.extend({
+			middleTag: 'ul', //包列表项的标签
+			itemTag: 'li', //列表项的标签
+			itemHeight: 30, //每一项的高度
+			to: 'bottom', //滚动方向，'bottom'向下滚动消息，'top'向下滚动消息
+			max: 50, //容器最多条数，超过后将自动清空
+		}, config);
+		middleStyle[this.config.to] = 0;
+		this.$box_ul = $('<' + this.config.middleTag + '/>').css(middleStyle);
+		this.$box = $box.css(boxStyle).html(this.$box_ul);
+		callback && callback($box, config);
+	};
+	turnMsg.prototype.addText = function(text, callback){
+		var config = this.config,
+				to = config.to,
+				max = config.max,
+				method = to === 'bottom' ? 'prepend' : 'append', //根据滚动的方向，使用不同的插入html方法
+				$box = this.$box,
+				$box_ul = this.$box_ul,
+				height = config.itemHeight,
+				html = this.getHtml(config.itemTag, text, height),
+				li_num = $box_ul.find(config.itemTag).length;
+		if(!(typeof html === "string" ? html.trim() : html)) return;
+		if($box.css("height") !== height + "px"){
+			$box.animate({
+				height : height
+			});
+		}
+		if(li_num > max) {
+			$box_ul.empty()[method](html).css(to, 0);
+		} else if(li_num > 0) {
+			$box_ul[method](html).css(to, (-li_num * height) + "px");
+		} else {
+			$box_ul[method](html);
+		};
+		callback && callback($box, text, li_num);
+		return this;
+	};
+	turnMsg.prototype.getHtml = function(tag, text, height){
+		return '<' + tag + ' style="height:' + height + 'px; line-height:' + height + 'px;">' + text + '</' + tag + '>';
+	}
+	turnMsg.prototype.empty = function(callback){
+		var that = this,
+				to = this.config.to,
+				$box = that.$box,
+				$box_ul = that.$box_ul;
+		$box.animate({
+			height : 0
+		},function() {
+			$box_ul.empty().css(to, 0);
+			callback && callback(that);
+		});
+		return that;
+	}
+	window.turnMsg = turnMsg;
+})(jQuery);
